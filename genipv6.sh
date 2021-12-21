1999aae2166c7d5c824385b8dd87c72719a72847
alathuan2 <banay.thuan@gmail.com>
2021-12-21 16:09:45 +0700

    Update genipv6.sh

--- genipv6.sh
+++ genipv6.sh
@@ -58,7 +58,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "thuantran/thuantranv6/$IP4/$port/$(gen64 $IP6)"
+        echo "thuantran/thuantran/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
